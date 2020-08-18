5918216f7ae7b872df7d4f6f6628136bf92c3d3c
mengfangui <meng010387@126.com>
2020-08-18 14:11:12 +0800

    '添加示例文件'

--- packages/pkg3/lib/pkg3.ts
+++ packages/pkg3/lib/pkg3.ts
@@ -1,3 +1,9 @@
+// 引入dayjs
+import dayjs from 'dayjs'
+// 引入lodash
+import * as _ from "lodash";
+console.log(_.padStart("Hello TypeScript!", 20, " "),'===lodash示例===')
+console.log(dayjs().format(),'===dayjs示例===')
 function greeter(person:string) {
     return "Hello, " + person;
 }
